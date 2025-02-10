adf016ea5bdc112ee4c40752e57ffdf010675699
Phat Ca <phatca@hawaii.edu>
2025-02-09 23:34:37 -1000

    modified:   src/components/AddStuffForm.tsx         modified:   src/lib/dbActions.ts         modified:   src/lib/validationSchemas.ts

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,18 +1,18 @@
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-  income: Yup.number().required(),
-  expenses: Yup.number().required(),
-});
+// export const AddStuffSchema = Yup.object({
+//   name: Yup.string().required(),
+//   quantity: Yup.number().positive().required(),
+//   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
+//   owner: Yup.string().required(),
+//   income: Yup.number().required(),
+//   expenses: Yup.number().required(),
+// });
 
-export const EditStuffSchema = Yup.object({
-  id: Yup.number().required(),
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
+// export const EditStuffSchema = Yup.object({
+//   id: Yup.number().required(),
+//   name: Yup.string().required(),
+//   quantity: Yup.number().positive().required(),
+//   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
+//   owner: Yup.string().required(),
+// });
